3fa8bc901ba69937f54658ae140acd7aca053ca0
Magda Balun <magi@op.pl>
2018-01-06 00:08:20 +0100

    Change index.js file

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var colors = require('colors');
 fs.readdir('./',  function(err, files) {
     fs.writeFile('./files.txt', files, function(err) {
         if(err) throw err;
+        console.log("Zapisano!");
     });
-    console.log("Zapisano!");
 });
 
 
